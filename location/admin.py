4d37157bd82072ba455959767d1c45fe41194275
davidinmichael <davidinmichael@gmail.com>
2023-10-25 11:37:44 +0100

    I made the first migrations

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Continent)
+admin.site.register(Currency)
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(LGA)
+admin.site.register(Address)
