a975b80e129def429ca0953e17a1f6942d67eafe
davidinmichael <davidinmichael@gmail.com>
2023-10-25 11:34:26 +0100

    I added the location models and serializers

--- location/models.py
+++ location/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Continent(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    abbreviation = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name} | {self.abbreviation}"
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+    capital = models.CharField(max_length=100, unique=True)
+    currency = models.ForeignKey(
+        Currency, blank=True, null=True, on_delete=models.SET_NULL)
+    continent = models.ForeignKey(
+        Continent, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name} | {self.capital}"
+
+
+class State(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    capital = models.CharField(max_length=100, unique=True)
+    country = models.ForeignKey(Country, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name} | {self.capital}"
+
+
+class LGA(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    state = models.ForeignKey(State, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} | {self.state}"
+
+
+class Address(models.Model):
+    address = models.CharField(max_length=100)
+    town = models.CharField(max_length=100)
+    lga = models.ForeignKey(LGA, null=True, on_delete=models.SET_NULL)
+    state = models.ForeignKey(State, null=True, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.town} | {self.state}, {self.country}"
---
+++ location/serializers.py
@@ -0,0 +1,105 @@
+from rest_framework import serializers
+from .models import *
+
+
+class ContinentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Continent
+        fields = ["name", "abbreviation"]
+
+
+class CurrencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = "__all__"
+
+    extra_kwargs = {
+        "id": {"read_only": True}
+    }
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = "__all__"
+
+    extra_kwargs = {
+        "id": {"read_only": True}
+    }
+
+    def validate_currency(self, value):
+        try:
+            currency = Currency.objects.get(abbreviation=value)
+        except Currency.DoesNotExist:
+            raise serializers.ValidationError({"message": "Invalid currency"})
+        return value
+
+    def validate_continent(self, value):
+        try:
+            continent = Continent.objects.get(name=value)
+        except Continent.DoesNotExist:
+            raise serializers.ValidationError(
+                {"message": "Invalid continent"})
+        return value
+
+
+class StateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = "__all__"
+
+    extra_kwargs = {
+        "id": {"read_only": True}
+    }
+
+    def validate_country(self, value):
+        try:
+            country = Country.objects.get(name=value)
+        except Country.DoesNotExist:
+            raise serializers.ValidationError({"message": "Invalid Country"})
+        return value
+
+
+class LGASerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = LGA
+        fields = ["name", "state"]
+
+    def validate_state(self, value):
+        try:
+            state = State.objects.get(name=value)
+        except State.DoesNotExist:
+            raise serializers.ValidationError({"message": "Invalid State"})
+        return value
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
+
+    extra_kwargs = {
+        "id": {"read_only": True}
+    }
+
+    def validate_lga(self, value):
+        try:
+            lga = LGA.objects.get(name=value)
+        except LGA.DoesNotExist:
+            raise serializers.ValidationError({"message": "Invalid LGA"})
+        return value
+
+    def validate_state(self, value):
+        try:
+            state = State.objects.get(name=value)
+        except State.DoesNotExist:
+            raise serializers.ValidationError({"message": "Invalid State"})
+        return value
+
+    def validate_country(self, value):
+        try:
+            country = Country.objects.get(name=value)
+        except Country.DoesNotExist:
+            raise serializers.ValidationError({"message": "Invalid Country"})
+        return value
